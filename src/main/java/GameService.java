64bb2ec54489e561240c0241bba2bdd1b725f65c
Laiza Velez <lminelly6@gmail.com>
2024-04-29 18:49:38 +0000

    Done

--- src/main/java/GameService.java
+++ src/main/java/GameService.java
@@ -27,6 +27,9 @@ public class GameService {
      * @return true if guessNumber is equal to the secretNumber, false otherwise.
      */
     public boolean checkGuess(double guessNumber) {
+        if (secretNumber==guessNumber){
+            return true;
+        }
         return false;
     }
 }
